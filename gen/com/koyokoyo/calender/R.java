21c45a0907a31a3ea2b821d6cd9fc3773bcba59b
あつこ <colorful.circle.42@gmail.com>
2012-05-16 23:50:25 +0900

    月の横にボタンつけた。リダイレクト方法考え中

--- gen/com/koyokoyo/calender/R.java
+++ gen/com/koyokoyo/calender/R.java
@@ -10,31 +10,50 @@ package com.koyokoyo.calender;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040001;
+        public static final int olive=0x7f040002;
+        public static final int red=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int date=0x7f050004;
-        public static final int dayOfTheWeek=0x7f050003;
-        public static final int month=0x7f050002;
-        public static final int title=0x7f050000;
-        public static final int year=0x7f050001;
+        public static final int currentMonth=0x7f060006;
+        public static final int date=0x7f060004;
+        public static final int down=0x7f060005;
+        public static final int fri=0x7f06000d;
+        public static final int mon=0x7f060009;
+        public static final int month=0x7f060002;
+        public static final int sat=0x7f06000e;
+        public static final int sun=0x7f060008;
+        public static final int thu=0x7f06000c;
+        public static final int title=0x7f060000;
+        public static final int tue=0x7f06000a;
+        public static final int up=0x7f060007;
+        public static final int wed=0x7f06000b;
+        public static final int week=0x7f060003;
+        public static final int year=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int month=0x7f030001;
+        public static final int week=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int date=0x7f04000b;
-        public static final int fri=0x7f040007;
-        public static final int mon=0x7f040003;
-        public static final int month=0x7f04000a;
-        public static final int sat=0x7f040008;
-        public static final int sun=0x7f040009;
-        public static final int thu=0x7f040006;
-        public static final int title=0x7f040001;
-        public static final int tue=0x7f040004;
-        public static final int wed=0x7f040005;
-        public static final int year=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int date=0x7f05000b;
+        public static final int down=0x7f05000d;
+        public static final int fri=0x7f050007;
+        public static final int mon=0x7f050003;
+        public static final int month=0x7f05000a;
+        public static final int sat=0x7f050008;
+        public static final int sun=0x7f050009;
+        public static final int thu=0x7f050006;
+        public static final int title=0x7f050001;
+        public static final int tue=0x7f050004;
+        public static final int up=0x7f05000c;
+        public static final int wed=0x7f050005;
+        public static final int year=0x7f050002;
     }
 }
--- src/com/koyokoyo/calender/KoyokoyoActivity.java
+++ src/com/koyokoyo/calender/KoyokoyoActivity.java
@@ -7,6 +7,8 @@ import com.koyokoyo.calender.R.id;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -19,6 +21,10 @@ public class KoyokoyoActivity extends Activity {
 	 * 
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
+	
+	
+	
+	int monthCount =0;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,26 +34,76 @@ public class KoyokoyoActivity extends Activity {
 		int month = Calendar.getInstance().get(Calendar.MONTH)+1;
 		MonthlyCalendar monthlyCalendar = new MonthlyCalendar(currentYear,
 				month);
-		ArrayList<String> dayOfTheWeekList = new ArrayList<String>();
 		ArrayList<String> dateList = monthlyCalendar.calcFields();
-
-		dayOfTheWeekList.add("日");
-		dayOfTheWeekList.add("月");
-		dayOfTheWeekList.add("火");
-		dayOfTheWeekList.add("水");
-		dayOfTheWeekList.add("木");
-		dayOfTheWeekList.add("金");
-		dayOfTheWeekList.add("土");
+		
 
 
 		TextView titleView = (TextView) findViewById(id.title);
 		TextView yearView = (TextView) findViewById(id.year);
-		TextView monthView = (TextView) findViewById(id.month);
+		TextView monthView = (TextView) findViewById(id.currentMonth);
+		TextView upView = (TextView) findViewById(id.up);
+		TextView downView = (TextView) findViewById(id.down);
+	
 		// テキストビューのテキストを設定します
 		titleView.setText(R.string.title);
 	
 		String monthName = null;
-		switch(Calendar.getInstance().get(Calendar.MONTH)+1){
+		//int currenMonth = Calendar.getInstance().get(Calendar.MONTH)+1;
+		
+		upView.setText(R.string.up);
+		downView.setText(R.string.down);
+
+		
+		monthName = monthSwitch(monthCount);
+
+		yearView.append(String.valueOf(Calendar.getInstance().get(Calendar.YEAR)));
+		monthView.append(monthName);
+		
+		TextView monView = (TextView) findViewById(id.mon);
+		TextView tueView = (TextView) findViewById(id.tue);
+		TextView wedView = (TextView) findViewById(id.wed);
+		TextView thuView = (TextView) findViewById(id.thu);
+		TextView friView = (TextView) findViewById(id.fri);
+		TextView satView = (TextView) findViewById(id.sat);
+		TextView sunView = (TextView) findViewById(id.sun);
+		
+		monView.setText(R.string.mon);
+		tueView.setText(R.string.tue);
+		wedView.setText(R.string.wed);
+		thuView.setText(R.string.thu);
+		friView.setText(R.string.fri);
+		satView.setText(R.string.sat);
+		sunView.setText(R.string.sun);
+
+		ArrayAdapter<String> dateAdapter = new ArrayAdapter<String>(
+				getApplicationContext(), android.R.layout.simple_list_item_1,
+				dateList);
+
+		GridView gridViewDate = (GridView) findViewById(R.id.date);
+		gridViewDate.setAdapter(dateAdapter);
+		
+		
+	}
+	
+
+	public void onClickButtonUp(View view){
+		monthCount++;
+		monthSwitch(monthCount);
+		Log.i("Next", String.valueOf(monthCount));
+		Log.i("NextMonth", monthSwitch(monthCount));
+		
+    }
+	
+	public void onClickButtonDown(View view){
+		monthCount--;
+		monthSwitch(monthCount);
+		Log.i("Previous", String.valueOf(monthCount));
+		Log.i("PreviousMonth", monthSwitch(monthCount));
+    }
+
+	private String monthSwitch(int monthCount) {
+		String monthName = null;
+		switch(Calendar.getInstance().get(Calendar.MONTH)+1+monthCount){
 		case 1:
 			monthName = "January";
 			break;
@@ -85,22 +141,7 @@ public class KoyokoyoActivity extends Activity {
 			monthName = "December";
 			break;
 		}
-
-		yearView.append(String.valueOf(Calendar.getInstance().get(Calendar.YEAR)));
-		monthView.append(monthName);
-
-		ArrayAdapter<String> dayOfTheWeekAdapter = new ArrayAdapter<String>(
-				getApplicationContext(), android.R.layout.simple_list_item_1,
-				dayOfTheWeekList);
-
-		GridView gridViewDayOfTheWeek = (GridView) findViewById(R.id.dayOfTheWeek);
-		gridViewDayOfTheWeek.setAdapter(dayOfTheWeekAdapter);
-
-		ArrayAdapter<String> dateAdapter = new ArrayAdapter<String>(
-				getApplicationContext(), android.R.layout.simple_list_item_1,
-				dateList);
-
-		GridView gridViewDate = (GridView) findViewById(R.id.date);
-		gridViewDate.setAdapter(dateAdapter);
+		return monthName;
 	}
+	
 }
